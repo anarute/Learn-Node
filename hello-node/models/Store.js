94402272936189b12bcb90aa06b10d0b42145226
Ana Rute <anarutecvm@gmail.com>
2017-07-17 20:51:33 -0300

    add store schema

--- hello-node/models/Store.js
+++ hello-node/models/Store.js
@@ -3,5 +3,27 @@ mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-  
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
 });
+
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')){
+    next();
+    return;
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO check if slug is unique
+})
+
+module.exports = mongoose.model('Store', storeSchema);
